090a7a1c9eb59fa80691cb330a106f7f1b9992c5
tiagoadonis <tiago.adonis@ua.pt>
2022-05-18 19:54:33 +0100

    raspberry app init code

--- App_RPi/app_raspberry.py
+++ App_RPi/app_raspberry.py
@@ -1,52 +1,82 @@
 import RPi.GPIO as GPIO
 import time
- 
-#GPIO Mode (BOARD / BCM)
+
+# Global configurations
 GPIO.setmode(GPIO.BCM)
- 
-#set GPIO Pins
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
- 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
+GPIO.setwarnings(False)
+
+# Ultrasonic configuration
+TRIG = 18 
+ECHO = 24
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+# LED configuration
+LED_PIN = 8
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+print("Distance Measurement In Progress")
+
+try:
+  while True:
+    # Force the trigger to false
+    GPIO.output(TRIG, False)
+    print("Waiting For Sensor To Settle")
+    
+    # TODO check this sleep time
+    # sleep for 2s
+    time.sleep(2)
+
+    # Init the trigger
+    GPIO.output(TRIG, True)
+    # sleep for 0,01s
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+
+    # Close the trigger
+    GPIO.output(TRIG, False)
+
+    # Save the start time
+    while GPIO.input(ECHO) == 0:
+      startTime = time.time()
+
+    # Get the end time
+    while GPIO.input(ECHO) == 1:
+      endTime = time.time()
+
+    # Calculate the time elapsed 
+    timeElapsed = endTime - startTime
+
+    # Calculate the distance: 
+    # - multiple by the sonic speed (34300 cm/s)
+    # - divide by 2, the going and coming path of the pulse
+    distance = (timeElapsed * 34300) / 2
+
+    # Round the distance value with two decimal number
+    distance = round(distance, 2)
+    
+    # If the distance is bigger than 15cm the led changes to high level
+    if(distance > 15):
+      GPIO.output(LED_PIN, GPIO.HIGH)
+      # sleep for 1s
+      time.sleep(1)
+    # else the led stays on low
+    else:
+      GPIO.output(LED_PIN, GPIO.LOW)
+      # sleep for 1s
+      time.sleep(1)
+
+    #TODO try this code block to blink with the distance
+    # GPIO.output(LED_PIN, GPIO.HIGH)
+    # time.sleep(distance * 0.02)
+    # GPIO.output(LED_PIN, GPIO.LOW)
+    # time.sleep(distance * 0.02)
+
+    # Print the distance value in cm
+    print("Distance: ", distance, "cm")
+
+  GPIO.cleanup()
+
+# If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+except KeyboardInterrupt: 
+    print("Cleaning up!")
+    gpio.cleanup()
